e26a4ec482c1d199a324b0d9bba944539701320a
Michal Moskal <michal@moskal.me>
2022-08-19 15:47:14 +0200

    add button handling and screen pins

--- libs/screen---st7735/screen.cpp
+++ libs/screen---st7735/screen.cpp
@@ -9,6 +9,159 @@
 
 #include "jddisplay.h"
 
+#define LOOKUP_PIN(x)  X_PIN_ ## x
+#define X_PIN_DISPLAY_MISO (&uBit.io.P14)
+#define X_PIN_DISPLAY_MOSI (&uBit.io.P15)
+#define X_PIN_DISPLAY_SCK (&uBit.io.P13)
+#define X_PIN_DISPLAY_BL (&uBit.io.P19)
+#define X_PIN_DISPLAY_RST (&uBit.io.P16)
+#define X_PIN_DISPLAY_DC (&uBit.io.P8)
+#define X_PIN_DISPLAY_CS ((codal::Pin*)NULL)
+
+#define X_PIN_BTNMX_LATCH (&uBit.io.P9)
+#define X_PIN_BTNMX_CLOCK (&uBit.io.P20)
+#define X_PIN_BTNMX_DATA (&uBit.io.P14)
+
+#define CODAL_SPI NRF52SPI
+
+
+namespace pxt {
+
+static void waitABit() {
+    // for (int i = 0; i < 10; ++i)
+    //    asm volatile("nop");
+}
+
+class ButtonMultiplexer : public CodalComponent {
+  public:
+    Pin &latch;
+    Pin &clock;
+    Pin &data;
+    uint32_t state;
+    uint32_t invMask;
+    uint16_t buttonIdPerBit[8];
+    bool enabled;
+
+    ButtonMultiplexer(uint16_t id)
+        : latch(*LOOKUP_PIN(BTNMX_LATCH)), clock(*LOOKUP_PIN(BTNMX_CLOCK)),
+          data(*LOOKUP_PIN(BTNMX_DATA)) {
+        this->id = id;
+        this->status |= DEVICE_COMPONENT_STATUS_SYSTEM_TICK;
+
+        state = 0;
+        invMask = 0;
+        enabled = true;
+
+        memset(buttonIdPerBit, 0, sizeof(buttonIdPerBit));
+
+        data.getDigitalValue(PullMode::Down);
+        latch.setDigitalValue(1);
+        clock.setDigitalValue(1);
+    }
+
+    void disable() {
+        data.getDigitalValue(PullMode::None);
+        latch.getDigitalValue(PullMode::None);
+        clock.getDigitalValue(PullMode::None);
+        enabled = false;
+    }
+
+    bool isButtonPressed(int id) {
+        for (int i = 0; i < 8; ++i) {
+            if (buttonIdPerBit[i] == id)
+                return (state & (1 << i)) != 0;
+        }
+        return false;
+    }
+
+    uint32_t readBits(int bits) {
+        latch.setDigitalValue(0);
+        waitABit();
+        latch.setDigitalValue(1);
+        waitABit();
+
+        uint32_t state = 0;
+        for (int i = 0; i < bits; i++) {
+            state <<= 1;
+            if (data.getDigitalValue(PullMode::Down))
+                state |= 1;
+
+            clock.setDigitalValue(0);
+            waitABit();
+            clock.setDigitalValue(1);
+            waitABit();
+        }
+
+        return state;
+    }
+
+    virtual void periodicCallback() override {
+        if (!enabled)
+            return;
+
+        uint32_t newState = readBits(8);
+        newState ^= invMask;
+        if (newState == state)
+            return;
+
+        for (int i = 0; i < 8; ++i) {
+            uint32_t mask = 1 << i;
+            if (!buttonIdPerBit[i])
+                continue;
+            int ev = 0;
+            if (!(state & mask) && (newState & mask))
+                ev = PXT_INTERNAL_KEY_DOWN;
+            else if ((state & mask) && !(newState & mask))
+                ev = PXT_INTERNAL_KEY_UP;
+            if (ev) {
+                Event(ev, buttonIdPerBit[i]);
+                Event(ev, 0); // any key
+            }
+        }
+
+        state = newState;
+    }
+};
+
+static ButtonMultiplexer *btnMultiplexer;
+ButtonMultiplexer *getMultiplexer() {
+    if (!btnMultiplexer)
+        btnMultiplexer = new ButtonMultiplexer(DEVICE_ID_FIRST_BUTTON);
+    return btnMultiplexer;
+}
+
+int registerMultiplexedButton(int pin, int buttonId) {
+    if (1050 <= pin && pin < 1058) {
+        pin -= 50;
+        getMultiplexer()->invMask |= 1 << (pin - 1000);
+    }
+    if (1000 <= pin && pin < 1008) {
+        getMultiplexer()->buttonIdPerBit[pin - 1000] = buttonId;
+        return 1;
+    }
+    return 0;
+}
+
+int multiplexedButtonIsPressed(int btnId) {
+    if (btnMultiplexer)
+        return btnMultiplexer->isButtonPressed(btnId) ? 512 : 0;
+    return 0;
+}
+
+//% expose
+uint32_t readButtonMultiplexer(int bits) {
+    if (!LOOKUP_PIN(BTNMX_CLOCK))
+        return 0;
+    return getMultiplexer()->readBits(bits);
+}
+
+void disableButtonMultiplexer() {
+    if (LOOKUP_PIN(BTNMX_CLOCK)) {
+        getMultiplexer()->disable();
+    }
+}
+}
+
 namespace pxt {
 
 class WDisplay {
@@ -37,7 +190,7 @@ class WDisplay {
         uint32_t cfg0 = getConfig(CFG_DISPLAY_CFG0, 0x40);
         uint32_t frmctr1 = getConfig(CFG_DISPLAY_CFG1, 0x000603);
 
-        int dispTp = getConfig(CFG_DISPLAY_TYPE, DISPLAY_TYPE_ST7735);
+        int dispTp = getConfig(CFG_DISPLAY_TYPE, DISPLAY_TYPE_SMART);
 
         doubleSize = false;
         smart = NULL;
@@ -219,7 +372,7 @@ class WDisplay {
     }
 };
 
-SINGLETON_IF_PIN(WDisplay, DISPLAY_MOSI);
+SINGLETON(WDisplay);
 
 //%
 int setScreenBrightnessSupported() {
